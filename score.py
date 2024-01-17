e24b252816660dcd5355fc21fe4751882d83eb1a
Harry Sharma <96082317+harrysharma1@users.noreply.github.com>
2024-01-17 13:19:13 +0000

    Update score.py

--- score.py
+++ score.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 
-
 scores = [6,9,7,5,9,7,8,9,10,9,7,9,10,7,6,4,6,8,9,7,8,7,5,8,7,7,8,3,8,7,8,3,7,8,8,7,8,5,7,7,5,7]
 day = [i+1 for i in range(len(scores))]
 plt.plot(day, scores, color='red', marker='o')
 plt.title('Productivity', fontsize=14)
-plt.xlabel('days (starting from 06-11-2023)', fontsize=14)
-plt.ylabel('feeling of productivity', fontsize=14)
+plt.xlabel('Days (starting from 06-11-2023)', fontsize=14)
+plt.ylabel('Productivity rating', fontsize=14)
 plt.ylim(0, 15)
 plt.grid(True)
-plt.show()
+
+
+plt.savefig("Figure_1.png")
