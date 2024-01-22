c4cc5d36fec62fc25b506d2465e236ccefaea2ed
Harry Sharma <96082317+harrysharma1@users.noreply.github.com>
2024-01-22 18:21:53 +0000

    Update score.py

--- score.py
+++ score.py
@@ -1,6 +1,12 @@
 import matplotlib.pyplot as plt
 
-scores = [6,9,7,5,9,7,8,9,10,9,7,9,10,7,6,4,6,8,9,7,8,7,5,8,7,7,8,3,8,7,8,3,7,8,8,7,8,5,7,7,5,7,6,7,8,9]
+csv_file = open('score.csv','r')
+score = (csv_file.read().split(","))
+scores =[]
+for i in score:
+    if i.isdigit():
+        scores.append(int(i))
+
 day = [i+1 for i in range(len(scores))]
 plt.plot(day, scores, color='red', marker='o')
 plt.title('Productivity', fontsize=14)
